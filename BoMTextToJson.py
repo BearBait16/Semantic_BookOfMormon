ba2f5da5abff21524ec3ac662e1674c873f3af04
BearBait16 <jacksonaden@gmail.com>
2024-08-21 20:38:11 -0600

    Started full config of the BoM text file, and did the first (unsuccessful) json creation. It only added one verse.

--- BoMTextToJson.py
+++ BoMTextToJson.py
@@ -1,10 +1,20 @@
-x = 0
+import json
 
+x = 0
+json_data = []
 with open ("Book_of_Mormon.txt") as file:
     content = file.read()
     verses_split_file = content.split('\n\n')
-    for verses in verses_split_file:
-        x += 1
 
-print(f"There are {x} verses in the Book of Mormon")
+for verses in verses_split_file:
+    lines = verses.split('\n', 1)
+    title = lines[0].strip()
+    text = lines[1].strip()
+    data = {
+        "Title": title,
+        "Text": text
+    }
+    json_data.append(data)
 
+with open('BomJson.json', 'w') as file:
+    json.dump(data, file)
