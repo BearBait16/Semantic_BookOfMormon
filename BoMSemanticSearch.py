3a2a800f252d7318225023d6da15cc4f38af17ab
BearBait16 <jacksonaden@gmail.com>
2024-08-23 18:23:22 -0600

    Added a .gitignore for the python venv. Also set up PgVector connection and BoMJson load in.

--- BoMSemanticSearch.py
+++ BoMSemanticSearch.py
@@ -1,1 +1,16 @@
-print("First Bit")
+import psycopg2
+import json
+
+BomJson =[]
+with open('BomJson.json') as file:
+    BomJson = json.load(file)
+
+print(BomJson)
+
+connection = psycopg2.connect(
+    dbname = "bom_vector",
+    user = "postgres",
+    password = "",
+    host = "localhost",
+    port = "5432"
+)
