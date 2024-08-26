56f5517b88e54dc85d220b51d3fa818a7417ec0a
BearBait16 <jacksonaden@gmail.com>
2024-08-25 21:18:05 -0600

    Added the code for Nomic embedding. Configured it to the json file. Have not run it though. Need to download dependencies.

--- BoMSemanticSearch.py
+++ BoMSemanticSearch.py
@@ -1,11 +1,10 @@
+import ollama
 import psycopg2
 import json
 
 BomJson =[]
 with open('BomJson.json') as file:
     BomJson = json.load(file)
-
-print(BomJson)
 
 connection = psycopg2.connect(
     dbname = "bom_vector",
@@ -14,3 +13,35 @@ connection = psycopg2.connect(
     host = "localhost",
     port = "5432"
 )
+
+embedder_model = "nomic-embed-text"
+
+def get_embedding(text_list):
+    embedding_list = []
+    vectors = ollama.embeddings(model=embedder_model, prompt= text_list)
+    embedding_list.append(vectors)
+    return embedding_list
+
+async def main():
+    insert_statement = """INSERT INTO embedded_docs (verse_title, text, embedding) VALUES (%s, %s, %s)"""
+
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS embedded_docs")
+    cursor.execute("""
+    CREATE TABLE embedded_docs (
+        id SERIAL PRIMARY KEY,
+        verse_title TEXT,
+        given_vector FLOAT8[],
+        doc_page TEXT
+    )
+""")
+    try:
+        for i, item in enumerate(BomJson):
+                title = item["Title"]
+                text = item["Text"]
+                vectors = get_embedding(item["Text"])
+                await cursor.execute(insert_statement, (title, text, vectors))
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
