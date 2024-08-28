0cd3ec08e1754b2634d646df5de10470968f4161
BearBait16 <jacksonaden@gmail.com>
2024-08-27 21:24:36 -0600

    I reorganized the functions so that they run with each other. The error I'm running into is that my machine is rejecting the connection. Will continue to troubleshoot.

--- BoMSemanticSearch.py
+++ BoMSemanticSearch.py
@@ -2,29 +2,8 @@ import ollama
 import psycopg2
 import json
 
-BomJson =[]
-with open('BomJson.json') as file:
-    BomJson = json.load(file)
-
-connection = psycopg2.connect(
-    dbname = "bom_vector",
-    user = "postgres",
-    password = "",
-    host = "localhost",
-    port = "5432"
-)
-
-embedder_model = "nomic-embed-text"
-
-def get_embedding(text_list):
-    embedding_list = []
-    vectors = ollama.embeddings(model=embedder_model, prompt= text_list)
-    embedding_list.append(vectors)
-    return embedding_list
-
-async def main():
-    insert_statement = """INSERT INTO embedded_docs (verse_title, text, embedding) VALUES (%s, %s, %s)"""
-
+async def InsertQuery(connection, BomJson):
+    insert_statement = """INSERT INTO embedded_docs (verse_title, given_vector, doc_page) VALUES (%s, %s, %s)"""
     cursor = connection.cursor()
     cursor.execute("DROP TABLE IF EXISTS embedded_docs")
     cursor.execute("""
@@ -35,13 +14,38 @@ async def main():
         doc_page TEXT
     )
 """)
-    try:
-        for i, item in enumerate(BomJson):
-                title = item["Title"]
-                text = item["Text"]
-                vectors = get_embedding(item["Text"])
-                await cursor.execute(insert_statement, (title, text, vectors))
-        connection.commit()
-    finally:
-        cursor.close()
-        connection.close()
+    for i, item in enumerate(BomJson):
+            title = item["Title"]
+            text = item["Text"]
+            vectors = get_embedding(item["Text"])
+            print("This works")
+            await cursor.execute(insert_statement, (title, text, vectors))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def get_embedding(text_list):
+    embedding_list = []
+    vectors = ollama.embeddings(model=embedder_model, prompt= text_list)
+    embedding_list.append(vectors)
+    return embedding_list
+
+"""
+Main Code
+"""
+
+embedder_model = "nomic-embed-text"
+BomJson =[]
+pg_connection = psycopg2.connect(
+    dbname = "bom_vector",
+    user = "postgres",
+    password = "FireN@tion1",
+    host = "localhost",
+    port = "5432"
+)
+
+with open('BomJson.json') as file:
+    BomJson = json.load(file)
+
+vector_embeddings = get_embedding(BomJson)
+InsertQuery(pg_connection, vector_embeddings)
